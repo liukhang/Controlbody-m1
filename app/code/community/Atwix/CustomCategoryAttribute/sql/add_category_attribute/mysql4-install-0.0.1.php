acf0a01d74cf5db39c8cc8928d55440e4f6c1d8d
liukhang <liukhang123@gmail.com>
2017-11-08 23:34:43 +0700

    up

---
+++ app/code/community/Atwix/CustomCategoryAttribute/sql/add_category_attribute/mysql4-install-0.0.1.php
@@ -0,0 +1,15 @@
+<?php
+$this->startSetup();
+$this->addAttribute(Mage_Catalog_Model_Category::ENTITY, 'custom_attribute', array(
+    'group'         => 'General Information',
+    'input'         => 'textarea',
+    'type'          => 'text',
+    'label'         => 'Custom attribute',
+    'backend'       => '',
+    'visible'       => true,
+    'required'      => false,
+    'visible_on_front' => true,
+    'global'        => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+));
+
+$this->endSetup();
